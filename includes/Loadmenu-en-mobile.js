6b2c1800a351b560f5218bbcfbf2154cd902643c
h4students98 <h4students98@gmail.com>
2024-08-12 22:27:34 +0200

    deleted all

--- includes/Loadmenu-en-mobile.js
+++
@@ -1,8 +0,0 @@
-document.write('<mynav><ul>' +
-    '<li><a href="about-en-mobile.php">About Me</a></li>' +
-    '<li ' + (activePage === 'Codinghome' ? 'class="active"' : '') + '><a href="Codinghome-en-mobile.php">Coding Projects</a></li>' +
-    '<li ' + (activePage === 'Physicshome' ? 'class="active"' : '') + '><a href="Physicshome-en-mobile.php">Physics Projects</a></li>' +
-    '<li ' + (activePage === 'Otherhome' ? 'class="active"' : '') + '><a href="Otherhome-en-mobile.php">Other Projects</a></li>' +
-    '</ul></mynav>');
-
-
--- includes/Loadmenu-en.js
+++
@@ -1,8 +0,0 @@
-document.write('<mynav><ul>' +
-    '<li><a href="about-en.php">About Me</a></li>' +
-    '<li ' + (activePage === 'Codinghome' ? 'class="active"' : '') + '><a href="Codinghome.php">Coding Projects</a></li>' +
-    '<li ' + (activePage === 'Physicshome' ? 'class="active"' : '') + '><a href="Physicshome.php">Physics Projects</a></li>' +
-    '<li ' + (activePage === 'Otherhome' ? 'class="active"' : '') + '><a href="Otherhome.php">Other Projects</a></li>' +
-    '</ul></mynav>');
-
-
