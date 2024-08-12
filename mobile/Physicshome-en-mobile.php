c862170a102927983b428520c05a4bb44ecc945a
h4students98 <h4students98@gmail.com>
2024-08-12 21:54:46 +0200

    Add files via upload

---
+++ mobile/Physicshome-en-mobile.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Physics Projects Colombini</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="../assets/css/stylemobile.css">
+</head>
+<body>
+
+	<script> var activePage = "Physicshome"; </script>  <!-- Set the active page as Physicshome -->
+	<script type="text/javascript" language="javascript" src="../includes/Loadmenu-en-mobile.js"></script>
+
+<div class="main-content">	<!-- ---START OF MAIN CONTENT--- -->
+	<div class="title">Projects about physics</div>
+	<div class="textmain">In this section, you will find presentations and text I have given in relation to various projects I have undertaken throughout the years during my studies.</div>
+	
+    
+        <div class="row">
+        	<div class="gradientbox_project">
+            <div class="col_proj_1">
+				<b>La spettroscopia Raman e gli inchiostri di Lucio Fontana</b>
+            </div>
+            <div class="col_proj_2">
+            	<a href="../downloads/pdf/Colombini_William_tesi.pdf"><img src="../assets/images/pdf_pic.svg" alt="download icon"></a>
+            	<span style="margin: 0 2%;"></span> <!-- Add a space between the images -->
+            	<a href="../downloads/ppt/Colombini_William_presentazione.pdf"><img src="../assets/images/ppt_pic.svg" alt="download icon"></a>
+            </div>
+        </div>
+    </div> 
+
+    <div class="spacebox"></div>
+
+
+        <div class="row">
+        	<div class="gradientbox_project">
+            <div class="col_proj_1">
+				<b>Photovoltaic:
+				a general overview
+				And a hypothetical case study</b>
+            </div>
+            <div class="col_proj_2">
+            	<a href="../downloads/pdf/Relazione_Colombini_Energetica_PV.pdf"><img src="../assets/images/pdf_pic.svg" alt="download icon"></a>
+            	<span style="margin: 0 2%;"></span> <!-- Add a space between the images -->
+            	<a href="../downloads/ppt/Presentazione_Energetica.pptx"><img src="../assets/images/ppt_pic.svg" alt="download icon"></a>
+            </div>
+        </div>
+    </div> 
+
+    <div class="spacebox"></div>
+
+</div>  <!-- ---END OF MAIN CONTENT--- -->
+	
+
+<footer class="footer">
+	<script type="text/javascript" language="javascript" src="../includes/Loadfooter-en.js"></script>
+</footer>
+</body>
+</html>
---
+++ mobile/about-en-mobile.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Homepage</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/stylehomemobile.css">
+</head>
+<body>
+
+
+
+<div class="main-content">    <!-- ---START OF MAIN CONTENT--- -->
+    <div class="gradientbox_line">
+        <!-- Language flags -->
+        <div class="language-icons">
+            <a href="#"><img src="../assets/images/english_pic.svg" alt="English" width="30" height="20"></a>
+            <a href="about-it-mobile.php"><img src="../assets/images/italy_pic.svg" alt="Italian" width="30" height="20"></a>
+            <a href="../desktop/about-en.php"><img src="../assets/images/desktop_icon.svg" alt="Desktop" width="50" height="50"></a>
+        </div>
+        <div class="row">
+            <div class="column1">
+                <img src="../assets/images/William_pic_cut.jpg" class="rounded-image">
+            </div>
+            <div class="column2">
+                <div class="textbox">
+                    <h1 style="font-family: 'Helvetica', sans-serif;">William Colombini</h1>
+                </div>
+            </div>
+        </div>
+        <div class="spacebox"></div>
+        <div class="row">
+            <p>I hold a <b>degree in Physics</b> from the <a href="https://fisica.dip.unipv.it/it">University of Pavia</a> and I am currently pursuing a <b>postgraduate degree in Material Physics</b> at the <a href="https://www.fisica.unimib.it/it">University of Milano-Bicocca</a>. The purpose of this website is to <b>showcase my various projects</b>, some of which are in Italian, and provide a platform for potential employers to learn more about my academic background and professional experience. </p>
+        </div>
+    </div>
+
+    <div class="spacebox"></div>
+
+    <div class="textbox_bg" id="coding-projects">
+        <h1>Coding Projects</h1>
+        <!-- Add the content inside a gradientbox_content div -->
+        <div class="gradientbox_content">
+            <p> This section features a comprehensive list of <b>explanatory slides and presentations</b> for selected projects I developed during years. You can contact me for more info or for the codes. </p>
+        </div>
+    </div> 
+
+    <div class="spacebox"></div>
+
+    <div class="textbox_bg" id="physics-projects">
+        <h1>Physics Projects</h1>
+        <!-- Add the content inside a gradientbox_content div -->
+        <div class="gradientbox_content">
+            <p>This section features a comprehensive list of <b>papers and presentation</b> produced during my physical studies. Additionally, the website showcases <b>my first thesis work</b>, which was completed at the end of my first degree. </p>
+        </div>
+    </div> 
+
+    <div class="spacebox"></div>
+
+    <div class="textbox_bg" id="other-projects">
+        <h1>Other Projects</h1>
+        <!-- Add the content inside a gradientbox_content div -->
+        <div class="gradientbox_content">
+            <p>I have attempted to organize non-official projects, such as <b>some lectures I have given</b> during my teaching period or some notes from my accademical studies</p>
+        </div>
+    </div> 
+
+</div>  <!-- ---END OF MAIN CONTENT--- -->
+
+<script>
+    // JavaScript to toggle the "active" class and change button text
+    var dropdowns = document.querySelectorAll('.textbox_bg');
+
+    dropdowns.forEach(function(dropdown) {
+        var content = dropdown.querySelector('.gradientbox_content');
+        var toggleButton = dropdown.querySelector('.toggle-button');
+        var buttonText = dropdown.querySelector('.button-text');
+
+        toggleButton.addEventListener('click', function() {
+            dropdown.classList.toggle('active');
+
+            if (dropdown.classList.contains('active')) {
+                buttonText.textContent = 'See less';
+            } else {
+                buttonText.textContent = 'Explore more';
+            }
+        });
+    });
+</script>
+
+<script type="text/javascript" src="../assets/js/Myboxeffects-en-mobile.js"></script>
+
+<footer class="footer">
+    <script type="text/javascript" language="javascript" src="../includes/Loadfooter-en.js"></script>
+</footer>
+</body>
+</html>
