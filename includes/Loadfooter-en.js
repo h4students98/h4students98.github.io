b1f76e13bb6f8eecc36e5942bae9518056b1da4c
h4students98 <h4students98@gmail.com>
2024-08-14 16:28:06 +0200

    Update Loadfooter-en.js

--- includes/Loadfooter-en.js
+++ includes/Loadfooter-en.js
@@ -1,5 +1,34 @@
  // document.write('<div class="blockfooter"> Website of William Colombini | Contact me: <i class="fa-solid fa-envelopes-bulk" style="color: #4598dc;"></i><a href=""mailto:ColombiniWilliam98@gmail.com" style="color: #FFB757;">ColombiniWilliam98@gmail.com</a> | Download my CV : <i class="fa-solid fa-newspaper" style="color: #3698da;"></i><a href="CV_William.pdf" download style="color: #FFB757;" style="text-decoration:none;">CVofWilliam.pdf</a> </div>');
-document.write('<div class="blockfooter"> Website of William Colombini | Contact me: <a href="mailto:h4students98@gmail.com" style="color: #FFB757;"> <img src="../assets/images/mail_pic.svg" alt="email icon" style="width: 1em; height: 1em; margin-right: 0.2em;"> h4students98@gmail.com</a> | <img src="../assets/images/phone_pic.svg" style="width: 1em; height: 1em; margin-right: 0.2em;"> +39 3519869993 | <img src="../assets/images/ig_pic.svg" style="width: 1em; height: 1em; margin-right: 0.2em;"> @willy.colo | Download CV : <a href="../downloads/pdf/CV_Colombini_eng.pdf" download style="color: #FFB757; text-decoration:none;"><img src="../assets/images/paper_pic.svg" alt="download icon" style="width: 1em; height: 1em; margin-right: 0.2em;">CV_Colombini_eng.pdf</a></div>')
+// Define styles for icons
+const iconStyle = 'style="width: 1em; height: 1em; margin-right: 0.2em;"';
+const linkStyle = 'style="color: #FFB757;"';
+const downloadLinkStyle = 'style="color: #FFB757; text-decoration:none;"';
+
+// Email section
+const emailIcon = `<img src="../assets/images/mail_pic.svg" alt="email icon" ${iconStyle}>`;
+const email = `<a href="mailto:h4students98@gmail.com" ${linkStyle}>${emailIcon}h4students98@gmail.com</a>`;
+
+// Phone section
+const phoneIcon = `<img src="../assets/images/phone_pic.svg" ${iconStyle}>`;
+const phone = `${phoneIcon} +39 3519869993`;
+
+// Instagram section
+const igIcon = `<img src="../assets/images/ig_pic.svg" ${iconStyle}>`;
+const instagram = `${igIcon} @willy.colo`;
+
+// CV Download section
+const paperIcon = `<img src="../assets/images/paper_pic.svg" alt="download icon" ${iconStyle}>`;
+const downloadCV = `<a href="../downloads/pdf/CV_Colombini_eng.pdf" download ${downloadLinkStyle}>${paperIcon}CV_Colombini_eng.pdf</a>`;
+
+// Combine all sections
+const footerContent = `
+  <div class="blockfooter">
+    Website of William Colombini | Contact me: ${email} | ${phone} | ${instagram} | Download CV: ${downloadCV}
+  </div>
+`;
+
+// Write the footer content to the document
+document.write(footerContent);
 
 // For making it work on firefox
  window.onload = function() {
