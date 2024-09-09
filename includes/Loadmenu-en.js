723d133473e38b75af873b27d10e8895ff693049
h4students98 <h4students98@gmail.com>
2024-09-09 18:10:19 +0200

    Delete includes directory

--- includes/Loadmenu-en.js
+++
@@ -1,8 +0,0 @@
-document.write('<mynav><ul>' +
-    '<li><a href="about-en.html">About Me</a></li>' +
-    '<li ' + (activePage === 'Codinghome' ? 'class="active"' : '') + '><a href="Codinghome.html">Coding Projects</a></li>' +
-    '<li ' + (activePage === 'Physicshome' ? 'class="active"' : '') + '><a href="Physicshome.html">Physics Projects</a></li>' +
-    '<li ' + (activePage === 'Otherhome' ? 'class="active"' : '') + '><a href="Otherhome.html">Other Projects</a></li>' +
-    '</ul></mynav>');
-
-
