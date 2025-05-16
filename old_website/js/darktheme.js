945bde8e4f336d63d9f650bed23242af9287e5a9
h4students98 <h4students98@gmail.com>
2025-05-17 00:01:54 +0200

    old website js

---
+++ old_website/js/darktheme.js
@@ -0,0 +1,24 @@
+// Ensure the dark theme is applied by default on page load
+      document.addEventListener('DOMContentLoaded', () => {
+        document.body.classList.add('dark-theme');
+        // Update the icon to reflect the current theme
+        darkthemeIconDesktop.src = "./assets/sun-icon.svg";
+        darkthemeIconHamburger.src = "./assets/sun-icon.svg";
+      });
+
+  // Function to toggle theme and icon
+  function toggleTheme(icon) {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+      icon.src = "./assets/sun-icon.svg";
+    } else {
+      icon.src = "./assets/moon-icon.svg";
+    }
+  }
+
+  // Add event listeners for both icons
+  const darkthemeIconDesktop = document.getElementById("darktheme-icon");
+  const darkthemeIconHamburger = document.getElementById("darktheme2-icon");
+
+  darkthemeIconDesktop.addEventListener('click', () => toggleTheme(darkthemeIconDesktop));
+  darkthemeIconHamburger.addEventListener('click', () => toggleTheme(darkthemeIconHamburger));
